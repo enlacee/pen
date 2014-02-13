70b5926e5feb9bc49515a9612c29648ebd8949da
Anibal Copitan <acopitan@gmail.com>
2014-02-13 16:33:17 -0500

    documentacion del pry

---
+++ docs/db/PENdb_base_1(drop).sql
@@ -0,0 +1,11 @@
+-- Tablas Base
+
+-- ELIMINAR TABLAS
+DROP TABLE objetivo_01;
+DROP TABLE objetivo_02;
+DROP TABLE objetivo_03;
+DROP TABLE objetivo_04;
+DROP TABLE objetivo_05;
+DROP TABLE objetivo_06;
+DROP TABLE fuentes_informacion;
+              
