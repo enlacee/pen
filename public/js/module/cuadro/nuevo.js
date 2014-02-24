17920d23585232a87662188a041c1041fb4c1620
Anibal Copitan <acopitan@gmail.com>
2014-02-24 15:27:09 -0500

    validacion de formulario (tipo de variable LISTA) CREAR cuadro

--- public/js/module/cuadro/nuevo.js
+++ public/js/module/cuadro/nuevo.js
@@ -9,10 +9,8 @@ $(function () {
                dataType: 'json',
                data: request,
                success: function (data) {
-                   console.log('data',data);
+                   //console.log('data',data);
                    response($.map(data, function (value, key) { 
-                       console.log('value',value);
-                       console.log('key',key);
                         return {
                             label: value,
                             value: value+':'+key,
@@ -26,9 +24,15 @@ $(function () {
     });
     
     $('#add').click(function() { 
-        var patron = /^([a-zA-Z ñ Ñ á Á éÉ íÍ óÓ úÚ 0-9]+)(:)(\d+)$/;
-        if (patron.test($variable.val())) {
+        //var patron = /^([a-zA-Z ñ Ñ á Á éÉ íÍ óÓ úÚ 0-9]+)(:)(\d+)$/;
+        var patron = /^([a-zA-Z\s]+)(\|)([A-Z]+)(:)(\d+)$/;
+        if (patron.test($variable.val())) {            
             var $input = '<input type="text" name="variableData[]" value="'+$variable.val()+'" readonly class="form-control input-sm"/>';
+            var s = getTipodeVariable($variable.val());            
+            if(getTipodeVariable($variable.val()) == 'LISTA') {
+                var checkbox = 'Multiple:<input type="checkbox" name="variableDataListaMultiple" value = "1" />';
+                $input += checkbox;
+            }
             $("#lista-variables").append('<p>'+$input+'</p>');
             $variable.val('')
                     .focus();
@@ -38,3 +42,13 @@ $(function () {
 
     });
 });
+
+
+// ----------------------- function de ayuda
+function getTipodeVariable(str) {
+    //var str = "sexo|LISTA:5";
+    var res = str.split(":",1);    
+    var string_tvariable = res[0];
+    var arreglo_tvariable = string_tvariable.split("|");    
+    return arreglo_tvariable[1];
+}
