092fef87d3069199ceadfcb81374cf340117613a
Anibal Copitan <acopitan@gmail.com>
2014-02-28 16:31:20 -0500

    jqgrid

--- application/controllers/adm_variable.php
+++ application/controllers/adm_variable.php
@@ -128,8 +128,7 @@ class Adm_variable extends MY_Controller {
         $limit = $this->input->get('rows');
         $sidx = $this->input->get('sidx');
         $sord = $this->input->get('sord');
-        if (!$sidx) $sidx = 1;
-        
+        if (!$sidx) $sidx = 1;        
 
         $dataGridString = null;
         if (isset($_GET['searchField']) && ($_GET['searchString'] != null)) {
@@ -176,72 +175,8 @@ class Adm_variable extends MY_Controller {
                 $row['fecha_registro']);
             $i++;        
         }
-        //echo json_encode($responce); 
+        
         $this->output->set_content_type('application/json');
-        $this->output->set_output(json_encode($responce));        
-        
-      
-        
-        
-  /*      
-        mysql_select_db($database) or die("Error conecting to db.");
-        $result = mysql_query("SELECT COUNT(*) AS count FROM invheader a, clients b WHERE a.client_id=b.client_id");
-        $row = mysql_fetch_array($result,MYSQL_ASSOC);
-        $count = $row['count'];
-
-        if( $count >0 ) {
-                $total_pages = ceil($count/$limit);
-        } else {
-                $total_pages = 0;
-        }
-        if ($page > $total_pages) $page=$total_pages;
-        $start = $limit*$page - $limit; // do not put $limit*($page - 1)
-        $SQL = "SELECT a.id, a.invdate, b.name, a.amount,a.tax,a.total,a.note FROM invheader a, clients b WHERE a.client_id=b.client_id ORDER BY $sidx $sord LIMIT $start , $limit";
-        $result = mysql_query( $SQL ) or die("Couldn t execute query.".mysql_error());
-*/
-        
-        
-        
-        
-        
-        
-        
-        /*
-        // ----
-        $page = $_GET['page']; // get the requested page
-        $limit = $_GET['rows']; // get how many rows we want to have into the grid
-        $sidx = $_GET['sidx']; // get index row - i.e. user click to sort
-        $sord = $_GET['sord']; // get the direction
-        if(!$sidx) $sidx =1;
-        // connect to the database
-        $db = mysql_connect($dbhost, $dbuser, $dbpassword)
-        or die("Connection Error: " . mysql_error());
-
-        mysql_select_db($database) or die("Error conecting to db.");
-        $result = mysql_query("SELECT COUNT(*) AS count FROM invheader a, clients b WHERE a.client_id=b.client_id");
-        $row = mysql_fetch_array($result,MYSQL_ASSOC);
-        $count = $row['count'];
-
-        if( $count >0 ) {
-                $total_pages = ceil($count/$limit);
-        } else {
-                $total_pages = 0;
-        }
-        if ($page > $total_pages) $page=$total_pages;
-        $start = $limit*$page - $limit; // do not put $limit*($page - 1)
-        $SQL = "SELECT a.id, a.invdate, b.name, a.amount,a.tax,a.total,a.note FROM invheader a, clients b WHERE a.client_id=b.client_id ORDER BY $sidx $sord LIMIT $start , $limit";
-        $result = mysql_query( $SQL ) or die("Couldn t execute query.".mysql_error());
-
-        $responce->page = $page;
-        $responce->total = $total_pages;
-        $responce->records = $count;
-        $i=0;
-        while($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
-            $responce->rows[$i]['id']=$row[id];
-            $responce->rows[$i]['cell']=array($row[id],$row[invdate],$row[name],$row[amount],$row[tax],$row[total],$row[note]);
-            $i++;
-        }        
-        echo json_encode($responce);   
-        */
+        $this->output->set_output(json_encode($responce));
     }
 }
