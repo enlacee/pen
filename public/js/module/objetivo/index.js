7d16e905bdff133cd745291cf19f9eac2ba41df3
Anibal Copitan <acopitan@gmail.com>
2014-03-03 13:39:12 -0500

    jqgrid edit + delete

--- public/js/module/objetivo/index.js
+++ public/js/module/objetivo/index.js
@@ -9,7 +9,7 @@ jQuery("#list").jqGrid({
    	colNames:['id_cuadro','Titulo', 'Fecha registro'],
    	colModel:[
    		{name:'id_cuadro',index:'id_cuadro', width:55},
-   		{name:'titulo',index:'titulo', width:400},   		
+   		{name:'titulo',index:'titulo', width:400, editable:true},   		
    		{name:'fecha_registro',index:'fecha_registro', width:90, align:"right"}
    	],
    	rowNum:10,
@@ -18,9 +18,28 @@ jQuery("#list").jqGrid({
    	sortname: 'id_cuadro',
     viewrecords: true,
     sortorder: "desc",
+    editurl:"/cuadro/jqeditar",
     /*caption:"JSON Example"*/
 });
-jQuery("#list").jqGrid('navGrid','#pager',{edit:false,add:false,del:false});
-
+//jQuery("#list").jqGrid('navGrid','#pager',{edit:true,add:false,del:false});
+//add delete
+$("#list").navGrid('#pager',
+        {edit:true,add:false,del:true,search:false},{}, {},
+        {width:500, url:'/cuadro/jqeditar',
+            reloadAfterSubmit:true,
+             onclickSubmit: function(param) {
+                var sr = jQuery('#list').getGridParam('selrow');
+                return {idUser:sr};
+            },
+            afterSubmit: function(reponse, data) {
+                if(reponse == 0) {
+                    alert("error no se puede eliminar");
+                }
+                $("#list").trigger('reloadGrid');
+                //$("#eData").click(); // clic sur "Annuler"
+                return [true,"Supression réussie"];
+            }
+        });
+// 
 //$("#list47").jqGrid('setGridWidth', 250);
 fixGridSize($("#list"));
